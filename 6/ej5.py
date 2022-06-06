e8b1165c494e2215226dca956061e99832792e73
anto144 <104173968+anto144@users.noreply.github.com>
2022-06-06 03:48:04 -0300

    Add files via upload

---
+++ 6/ej5.py
@@ -0,0 +1,12 @@
+from zope.interface import interface
+from zope.interface import implementer
+class Iinterface(interface):
+    def insertarElemento(posicion,elemento):
+        pass
+
+    def agregarElemento(elemento):
+        pass
+    
+    def mostrarElemento():
+        pass
+        
